ad8a5cebee6db5bd9096d0d236f979cc6cacbf84
Lucca Claro <luccaoc@al.insper.edu.br>
2022-05-26 15:42:23 -0300

    Jogo atualizado

--- Jogo.py
+++ Jogo.py
@@ -42,9 +42,26 @@ bg = pygame.transform.scale(bg,(WIDTH,HEIGHT))
 bg_rect = bg.get_rect()
 bg_rect2 = bg_rect.copy()
 
+botao_img = pygame.image.load('bola.png')
+
+
 def texto (text, fonte, text_col, x, y):
     img = fonte.render(text, True, text_col)
     window.blit(img,  (x,y))
+
+def reseta_jogo():
+    anel_grupo.empty
+    anel_grupo1.empty
+    anel_em_cima.kill()
+    anel_embaixo.kill()
+    ball.rect.x = 120
+    ball.rect.y = int(HEIGHT/2) + 8
+    asa_direita.rect.x = 183
+    asa_direita.rect.y = int(HEIGHT/2)-20
+    asa_esquerda.rect.x = 145
+    asa_esquerda.rect.y = int(HEIGHT/2)-15
+    placar = 0
+    return placar
 
 class Bola(pygame.sprite.Sprite):
     def __init__(self,x,y):
@@ -66,7 +83,7 @@ class Bola(pygame.sprite.Sprite):
                 self.rect.y += int(self.speedy)
 
        
-class Asa_esquerda(pygame.sprite.Sprite):
+class Asa(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
         self.images = []
@@ -112,8 +129,26 @@ class Anel(pygame.sprite.Sprite):
             self.rect.x += movimento_velocidade
         if self.rect.right < 0:
             self.kill()
+
+class Botao():
+    def __init__(self,x,y,image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+    
+    def draw(self):
+
+        acao = False
+
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                acao = True
+
+        window.blit(self.image,(self.rect.x, self.rect.y))
             
-            
+        return acao
 
 #grupos
 anel_grupo = pygame.sprite.Group()
@@ -123,8 +158,8 @@ bola_grupo = pygame.sprite.Group()
 asa_grupo_atras = pygame.sprite.GroupSingle()
 
 #posição dos sprites
-asa_direita = Asa_esquerda(183, int(HEIGHT/2)-20)
-asa_esquerda = Asa_esquerda(145, int(HEIGHT/2)-15)
+asa_direita = Asa(183, int(HEIGHT/2)-20)
+asa_esquerda = Asa(145, int(HEIGHT/2)-15)
 ball = Bola(150, int(HEIGHT/2))
 
 #adicionando os sprites aos grupos
@@ -132,6 +167,7 @@ asa_grupo_atras.add(asa_direita)
 asa_grupo.add(asa_esquerda)
 bola_grupo.add(ball)
 
+botao_reiniciar = Botao(WIDTH // 2 - 50, HEIGHT // 2 - 100, botao_img)
 
 #Loop principal
 game = True
@@ -164,7 +200,7 @@ while game:
         movimento_tela = False
 
     if ball.rect.top < 61:
-        game_over = False
+        game_over = True
         movimento_tela = False  
         voar = False
         continuar = False
@@ -201,6 +237,11 @@ while game:
             bg_rect.x += bg_rect.width
         bg_rect2.x = bg_rect.x + bg_rect2.width
     
+    if game_over == True:
+        if botao_reiniciar.draw() == True:
+            game_over = False
+            continuar = True
+            placar = reseta_jogo()
     
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
